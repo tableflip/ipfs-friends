3ddc3a8c00a26addbd489f079faa5987c8667b8d
Alan Shaw <alan.shaw@protocol.ai>
2018-11-23 22:57:58 +0000

    feat: version command and help text
    License: MIT
    Signed-off-by: Alan Shaw <alan.shaw@protocol.ai>

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,14 @@
 module.exports = function help () {
   const out = `
-It all starts with friends
+  Use the following commands to share files with your friends:
+
+  add  <Peer ID> <Name>  add a friend
+  dump <Name/Peer ID>    remove a friend
+  exit                   exit IPFS friends (also ctrl+c)
+  help                   print this help message
+  ls                     list your friends
+  share <CID> <Name>     ask friends to share the passed CID by the given name
+  version                print the version number of this tool
   `
   return { out }
 }
