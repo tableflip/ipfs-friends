a628fb73439406e84e1bdf3722ab3aa72c63d2f9
Alan Shaw <alan.shaw@protocol.ai>
2018-11-24 22:30:40 +0000

    docs: add help documentation for config commands
    License: MIT
    Signed-off-by: Alan Shaw <alan.shaw@protocol.ai>

--- commands/help.js
+++ commands/help.js
@@ -2,13 +2,15 @@ module.exports = function help () {
   const out = `
   Use the following commands to share files with your friends:
 
-  add  <Peer ID> <Name>  add a friend
-  dump <Name/Peer ID>    remove a friend
-  exit                   exit IPFS friends (also ctrl+c)
-  help                   print this help message
-  ls                     list your friends
-  share <CID> <Name>     ask friends to share the passed CID by the given name
-  version                print the version number of this tool
+  add  <Peer ID> <Name>     add a friend
+  config get [Key]          get all config or get a specifc config value for given key
+  config set <Key> <Value>  set a config key to the given value
+  dump <Name/Peer ID>       remove a friend
+  exit                      exit IPFS friends (also ctrl+c)
+  help                      print this help message
+  ls                        list your friends
+  share <CID> <Name>        ask friends to share the passed CID by the given name
+  version                   print the version number of this tool
   `
   return { out }
 }
